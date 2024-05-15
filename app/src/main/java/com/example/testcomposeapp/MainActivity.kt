3e4c5e4838a0e23c87519c24edb77282415d2363
v.iurkov <v.iurkov@vk.team>
2024-05-15 22:41:04 +0300

    refactor

--- app/src/main/java/com/example/testcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/testcomposeapp/MainActivity.kt
@@ -1,65 +1,20 @@
 package com.example.testcomposeapp
 
+import NavigationHost
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-
-const val HOME = "home"
-const val SETTINGS = "settings"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Column(Modifier.fillMaxSize()) {
-                val navigationController = rememberNavController()
-                NavHost(
-                    navController = navigationController,
-                    startDestination = HOME,
-                    modifier = Modifier.weight(1f)
-                ) {
-                    composable(HOME) { HomeScreen() }
-                    composable(SETTINGS) { SettingsScreen() }
-                }
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp),
-                    horizontalArrangement = Arrangement.SpaceEvenly
-                ) {
-                    Text(
-                        text = "Home Screen",
-                        modifier = Modifier.clickable { navigationController.navigate(HOME) })
-                    Text(
-                        text = "Settings Screen",
-                        modifier = Modifier.clickable { navigationController.navigate(SETTINGS) })
-                }
+                NavigationHost(modifier = Modifier.weight(1f))
             }
         }
     }
-}
-
-
-
-@Composable
-fun HomeScreen() {
-
-}
-
-@Composable
-fun SettingsScreen() {
 }
---
+++ app/src/main/java/com/example/testcomposeapp/navigation/NavHost.kt
@@ -0,0 +1,41 @@
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+const val HOME = "home"
+const val SETTINGS = "settings"
+@Composable
+fun NavigationHost(modifier: Modifier = Modifier) {
+    val navigationController = rememberNavController()
+
+    NavHost(
+        navController = navigationController,
+        startDestination = HOME,
+        modifier = modifier
+    ) {
+        composable(HOME) { HomeScreen() }
+        composable(SETTINGS) {  }
+    }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(
+            text = "Home Screen",
+            modifier = Modifier.clickable { navigationController.navigate(HOME) })
+        Text(
+            text = "Settings Screen",
+            modifier = Modifier.clickable { navigationController.navigate(SETTINGS) })
+    }
+}
